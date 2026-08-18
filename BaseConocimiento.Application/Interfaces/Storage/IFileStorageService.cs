dea8d99f46b0fc1c40553cc9e6c02d7aff48b629
SantanaPablo <pablobritez19@gmail.com>
2026-08-17 22:06:06 -0300

    Modelfile agregado para local

---
+++ BaseConocimiento.Application/Interfaces/Storage/IFileStorageService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseConocimiento.Application.Interfaces.Storage
+{
+    public interface IFileStorageService
+    {
+        Task<string> GuardarArchivoAsync(Guid manualId, Stream archivoStream, string nombreOriginal);
+        Task<bool> EliminarArchivoAsync(Guid manualId);
+        Task<Stream> ObtenerArchivoAsync(Guid manualId);
+        Task<bool> ExisteArchivoAsync(Guid manualId);
+    }
+}
